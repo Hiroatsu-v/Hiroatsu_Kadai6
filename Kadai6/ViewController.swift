f0f2c9ec39cac4fbb856a1167fb46b35d39313da
Hiroatsu <hiroatsu.7190@gmail.com>
2021-10-02 18:13:43 +0900

    Initial Commit

---
+++ Kadai6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai6
+//
+//  Created by Hiroatsu on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

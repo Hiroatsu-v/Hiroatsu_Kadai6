716b6b5b1b8ec8d7ab41febe0a7a84acb6bf71b5
Hiroatsu <hiroatsu.7190@gmail.com>
2021-10-04 10:23:00 +0900

    完成

--- Kadai6/ViewController.swift
+++ Kadai6/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+
+    private var randomNumber = Int.random(in: 1...100)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberLabel.text = String(randomNumber)
     }
 
+    @IBAction private func checkButton(_ sender: UIButton) {
+        let sliderValue = Int(slider.value)
+        if randomNumber == sliderValue {
+            presentAlert(message: "当たり!\nあなたの値:\(sliderValue)")
+        }
+        presentAlert(message: "ハズレ!\nあなたの値:\(sliderValue)")
+    }
 
+    func presentAlert(message: String) {
+        let alertController = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        let judgementAction = UIAlertAction(title: "再挑戦", style: .default) { ( _: UIAlertAction) in
+            self.randomNumber = Int.random(in: 1...100)
+            self.numberLabel.text = String(self.randomNumber)
+        }
+        alertController.addAction(judgementAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
